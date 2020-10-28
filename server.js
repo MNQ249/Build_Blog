471751991a99cdd8dbbb93afb678f111decf3c24
Mesfer AlQahtani <mralqahtani.t@stcs.com.sa>
2020-10-28 17:50:50 +0300

    add a new folder call articles included indexjs,newjs,formFields

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const articleRouter = require('./routes/articles');
+const express = require("express");
+const articleRouter = require("./routes/articles");
 const app = express();
 
-app.set('view engine', 'ejs') //converit ejs to html
+app.set("view engine", "ejs"); //converit ejs to html
 
-app.use('/articles',articleRouter);
+app.use("/articles", articleRouter);
 
-app.get('/', (req,res)=>{
-    res.render('index') // main page
-})
+app.get("/", (req, res) => {
+  const articles = [
+    {
+      title: "Test Article  1",
+      createAt: new Date(),
+      description: "Test for descriptions",
+    },
+    {
+      title: "Test Article  2",
+      createAt: new Date(),
+      description: "Test for descriptions 2",
+    },
+  ];
+  res.render("articles/index", { articles: articles }); // main page
+});
 
 app.listen(5000);
