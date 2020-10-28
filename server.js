5fbc7b15e8a0c155c992386578c9492619f805f9
Mesfer AlQahtani <mralqahtani.t@stcs.com.sa>
2020-10-28 14:00:11 +0300

    create folder router and create router each of router between serverjs and articlesjs

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const articleRouter = require('./routes/articles');
 const app = express();
 
 app.set('view engine', 'ejs') //converit ejs to html
 
+app.use('/articles',articleRouter);
+
 app.get('/', (req,res)=>{
-    res.render('index')
+    res.render('index') // main page
 })
 
 app.listen(5000);
