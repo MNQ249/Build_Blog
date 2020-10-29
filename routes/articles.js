c85a217b1798a767bd62c9f77f4dc3baf6cebec3
Mesfer AlQahtani <mralqahtani.t@stcs.com.sa>
2020-10-29 16:32:17 +0300

    create new articles, form  fields

--- routes/articles.js
+++ routes/articles.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  console.log("test articles")
+  
+})
+
 router.get("/new", (req, res) => {
-  res.render("/articles/new");
-});
+  console.log("test new test")
+  //res.send("test new")
+  res.render("articles/new");
+})
 
 router.post("/", (req, res) => {});
 
--- server.js
+++ server.js
@@ -7,16 +7,22 @@ app.set("view engine", "ejs"); //converit ejs to html
 app.use("/articles", articleRouter);
 
 app.get("/", (req, res) => {
+  console.log('test home page')
   const articles = [
     {
       title: "Test Article  1",
       createAt: new Date(),
-      description: "Test for descriptions",
+      description: "Test for descriptionssssss",
     },
     {
       title: "Test Article  2",
       createAt: new Date(),
-      description: "Test for descriptions 2",
+      description: "Test for descriptionsttttttt 2",
+    },
+    {
+      title: "Test Article  3",
+      createAt: new Date(),
+      description: "Test for descriptions 3",
     },
   ];
   res.render("articles/index", { articles: articles }); // main page
