eac2044e48dca4c6ba892f7fc520a355cd717ffa
Mesfer AlQahtani <mralqahtani.t@stcs.com.sa>
2020-11-01 19:07:51 +0300

    create a new show, edit router articles, and create a new schema

--- routes/articles.js
+++ routes/articles.js
@@ -1,17 +1,44 @@
 const express = require("express");
+const Article = require('./../models/article')
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  console.log("test articles")
+// router.get("/", (req, res) => {
+//   console.log("test articles")
   
-})
+// })
 
-router.get("/new", (req, res) => {
-  console.log("test new test")
+router.get('/new', (req, res) => {
+  // console.log("test new test")
   //res.send("test new")
-  res.render("articles/new");
+  res.render('articles/new', {article: new Article()} );
 })
 
-router.post("/", (req, res) => {});
+
+
+
+router.get('/:id', async (req,res)=> {
+  const article = await Article.findById(req.params.id)
+  if(article == null) res.redirect('/')
+  res.render('articles/show', { article:article})
+})
+
+
+
+
+router.post('/', async (req, res) => {
+  console.log(req.body.title)
+  let article = new Article({
+    title:req.body.title,
+    description:req.body.description,
+    markdown:req.body.markdown
+});
+try{
+  article = await article.save()
+  res.redirect(`/articles/${article.id}`)
+}catch (e){
+  console.log(e)
+  res.render('articles/new', {article: article})
+}
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,34 +1,39 @@
-const express = require("express");
-const mongoose = require("mongoose"); // to create a new database 
-const articleRouter = require("./routes/articles");
+const express = require('express');
+const mongoose = require('mongoose'); // to create a new database 
+const articleRouter = require('./routes/articles');
 const app = express();
 
-mongoose.connect('mongodb://localhost/blog',{ useNewUrlParser: true, useUnifiedTopology: true}); //connect our database
+mongoose.connect('mongodb://localhost/blog',
+{ useNewUrlParser: true, useUnifiedTopology: true}) //connect our database
 
-app.set("view engine", "ejs"); //converit ejs to html
+app.set('view engine', 'ejs'); // ejs work both html and js
 
-app.use("/articles", articleRouter);
+app.use(express.urlencoded({ extended: false }))
+
+
 
 app.get("/", (req, res) => {
-  console.log('test home page')
+  console.log('started home page')
   const articles = [
     {
-      title: "Test Article  1",
+      title: 'Test Article  1',
       createAt: new Date(),
-      description: "Test for descriptionssssss",
+      description: 'Test for descriptionssssss'
     },
     {
-      title: "Test Article  2",
+      title: 'Test Article  2',
       createAt: new Date(),
-      description: "Test for descriptionsttttttt 2",
+      description: 'Test for descriptionsttttttt 2'
     },
     {
-      title: "Test Article  3",
+      title: 'Test Article  3',
       createAt: new Date(),
-      description: "Test for descriptions 3",
-    },
+      description: 'Test for descriptions 3'
+    }
   ];
-  res.render("articles/index", { articles: articles }); // main page
+  res.render('articles/index', { articles: articles }); // main page
 });
+
+app.use('/articles', articleRouter);    
 
 app.listen(5000);
